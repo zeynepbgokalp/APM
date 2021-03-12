2420677ef4e49670924d6df777dfeb2ec787643b
zeynepbgokalp <zeynepgokalp@hotmail.com>
2021-03-12 22:48:39 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+/*import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template:
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component </div>
+      <pm-products> </pm-products>
+    </div>
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}*/
+
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular: Getting Started';
+}
+
+
